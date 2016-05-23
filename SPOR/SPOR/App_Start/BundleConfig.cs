4d6f124eef19e052ba25dad62ac8c9d8d25e5b40
austinw <austinwil24@gmail.com>
2016-05-23 11:16:23 -0400

    update config for bundling

--- SPOR/SPOR/App_Start/BundleConfig.cs
+++ SPOR/SPOR/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace SPOR
 {
@@ -9,39 +8,58 @@ namespace SPOR
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                    "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                    "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/Scripts/jquery-ui-{version}.js"));
+                    "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/sageui").Include(
-                "~/Scripts/sage-ui.js"));
+                    "~/Scripts/sage-ui.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                    "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                    "~/Scripts/bootstrap.js",
+                    "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-theme.css",
-                      "~/Content/site.css"));
+                    "~/Content/bootstrap.css",
+                    "~/Content/bootstrap-theme.css",
+                    "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/jquerybasecss").Include(
-                "~/Content/themes/base/all.css"));
+                    "~/Content/themes/base/all.css",
+                    "~/Content/themes/base/base.css",
+                    "~/Content/themes/base/core.css",
+                    "~/Content/themes/base/accordion.css",
+                    "~/Content/themes/base/autocomplete.css",
+                    "~/Content/themes/base/button.css",
+                    "~/Content/themes/base/datepicker.css",
+                    "~/Content/themes/base/dialog.css",
+                    "~/Content/themes/base/draggable.css",
+                    "~/Content/themes/base/menu.css",
+                    "~/Content/themes/base/progressbar.css",
+                    "~/Content/themes/base/resizable.css",
+                    "~/Content/themes/base/selectable.css",
+                    "~/Content/themes/base/selectmenu.css",
+                    "~/Content/themes/base/slider.css",
+                    "~/Content/themes/base/sortable.css",
+                    "~/Content/themes/base/spinner.css",
+                    "~/Content/themes/base/tabs.css",
+                    "~/Content/themes/base/tooltip.css",
+                    "~/Content/themes/base/theme.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/jquerytrontasticcss").Include(
-                "~/Content/themes/trontastic/jquery-ui.trontastic.css"));
+                    "~/Content/themes/trontastic/jquery-ui.trontastic.css"));
 
             bundles.Add(new StyleBundle("~/Content/sagecss").Include(
-                "~/Content/sage-ui.css"));
+                    "~/Content/sage-ui.css"));
         }
     }
 }
