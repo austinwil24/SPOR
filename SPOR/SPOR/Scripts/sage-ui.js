97e1ad3d055b75a49a9827b9353711c3259908e5
austinw <austinwil24@gmail.com>
2016-05-24 10:54:12 -0400

    adding navigation states for current page.

--- SPOR/SPOR/Scripts/sage-ui.js
+++ SPOR/SPOR/Scripts/sage-ui.js
@@ -1,6 +1,27 @@
 ﻿//SAGE UI JavaScript
 
-$(function () {
+$(document).ready(function () {
+
+    var loc = location.href.toLowerCase();
+    var isHomeCurrent = true;
+
+    $('.navbar-nav.sage a').each(function () {
+        var nav = $(this);
+        if (loc.indexOf(this.href.toLowerCase()) > -1) {
+            nav.addClass("current");
+            isHomeCurrent = false;
+        }
+        else {
+            nav.removeClass("current");
+        }
+    });
+
+    if (isHomeCurrent) {
+        $('#sagelogo').addClass("current");
+    }
+    else {
+        $('#sagelogo').removeClass("current");
+    }
 
     $(".datepicker").datepicker();
 
